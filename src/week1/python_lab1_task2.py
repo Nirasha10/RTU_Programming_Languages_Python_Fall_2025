7f665a9d3f71c76b5e4cb01ae3eb867a538d4419
Nirasha10 <nirashaprabashi01@gmail.com>
2025-11-01 16:33:47 +0200

    Update python_lab1_task2.py

--- src/week1/python_lab1_task2.py
+++ src/week1/python_lab1_task2.py
@@ -7,12 +7,14 @@ Write a function `greet_user(name)` that:
 - returns "Hello, <Name>! Welcome to Python!"
 Ask user for their name and print result.
 """
-
 def greet_user(name):
     """Return a greeting message after cleaning and capitalizing the name."""
     # TODO: implement cleaning and formatting
-    pass
+    cleaned_name = name.strip().capitalize()
+    return f"Hello, {cleaned_name}! Welcome to Python!"
 
 if __name__ == "__main__":
     # TODO: read name from input and print greeting
-    pass
+    name = input("Enter your name: ")
+    greeting = greet_user(name)
+    print(greeting)
